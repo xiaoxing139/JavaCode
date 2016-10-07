e7c3baca9b7ea71382c219992adeb65be9e2d6ed
xiaoxing <986738449@qq.com>
2016-10-07 15:25:21 +0800

    这是一个Struts2的测试项目

---
+++ struts2test1/src/com/struts2/action/loginAction.java
@@ -0,0 +1,9 @@
+package com.struts2.action;
+
+public class loginAction {
+
+	public String execute(){
+		System.out.println("µÇÂ¼³É¹¦");
+		return "success";
+	}
+}
---
+++ struts2test1/src/com/struts2/action/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Administrator
+ *
+ */
+package com.struts2.action;
